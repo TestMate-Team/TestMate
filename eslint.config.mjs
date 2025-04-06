da2e3cb459e66fa7ac7fbb644461e0fca09444bb
tomoya-n <s266227@wakayama-u.ac.jp>
2025-04-06 14:58:47 +0900

    eslintの設定を変更

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,10 @@
-import { defineConfig } from "eslint/config";
-import globals from "globals";
 import js from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
 import eslintConfigPrettier from "eslint-config-prettier";
+import pluginReact from "eslint-plugin-react";
 import pluginSimpleImportSort from "eslint-plugin-simple-import-sort";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
@@ -25,6 +25,7 @@ export default defineConfig([
       "simple-import-sort": pluginSimpleImportSort,
     },
     rules: {
+      "react/react-in-jsx-scope": "off",
       "simple-import-sort/imports": "error",
       "simple-import-sort/exports": "error",
       "@typescript-eslint/consistent-type-definitions": ["error", "interface"],
