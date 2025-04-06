1a4f8bcfd8cd10b9f9c67d955b916787ad33d7d5
tomoya-n <s266227@wakayama-u.ac.jp>
2025-04-06 12:55:46 +0900

    import順序を並び替えるものを追加

--- eslint.config.js
+++ eslint.config.js
@@ -3,12 +3,16 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
-import prettier from "eslint-config-prettier";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      "prettier",
+    ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
@@ -17,7 +21,7 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
-      prettier
+      "simple-import-sort": simpleImportSort,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -25,6 +29,9 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      // simple-import-sortのルールを追加
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
     },
   },
 )
