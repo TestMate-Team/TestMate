37c10e9bef916222fa3ab61712b89860f11e6e71
tomoya-n <s266227@wakayama-u.ac.jp>
2025-04-07 15:53:58 +0900

    tanstackをプロジェクト全体で使用できるように変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
 
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 import { Header } from "@/components/layout/Header";
 import { HomePage } from "@/page/home";
@@ -10,23 +10,21 @@ import { PostPage } from "@/page/posts";
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="flex flex-col min-h-screen items-center">
-        {/* モバイルでは最大幅448px、タブレットでは640px、デスクトップでは768pxに設定 */}
-        <div className="w-full max-w-md md:max-w-2xl lg:max-w-3xl relative bottom-1">
-          <Header />
-          <main className="pt-17 md:pt-20 px-4 shadow-md min-h-screen">
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/:id" element={<AppDetailPage />} />
-              <Route path="/posts" element={<PostPage />} />
-              {/* 404ページ */}
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </main>
-        </div>
+    <div className="flex flex-col min-h-screen items-center">
+      {/* モバイルでは最大幅448px、タブレットでは640px、デスクトップでは768pxに設定 */}
+      <div className="w-full max-w-md md:max-w-2xl lg:max-w-3xl relative bottom-1">
+        <Header />
+        <main className="pt-17 md:pt-20 px-4 shadow-md min-h-screen">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/:id" element={<AppDetailPage />} />
+            <Route path="/posts" element={<PostPage />} />
+            {/* 404ページ */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </main>
       </div>
-    </BrowserRouter>
+    </div>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,20 @@
 import "./index.css";
 
+import { QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
+import { queryClient } from "@/lib/queryClient";
 
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>,
 );
